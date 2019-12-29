a97ad61ecdbc132a31ebd77844c84a8998dc90fa
Okan Hollander <o.hollander1990@gmail.com>
2019-12-29 13:13:33 +0100

    adding list of owners to owners index page Closes #16

--- pet-clinic-web/src/main/java/com/okan/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/okan/petclinic/controllers/OwnerController.java
@@ -1,8 +1,14 @@
 package com.okan.petclinic.controllers;
 
+import com.okan.petclinic.model.Owner;
+import com.okan.petclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 /**
  * Author:   Okan Hollander
@@ -13,8 +19,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        Set<Owner> listOwners = ownerService.findAll();
+
+        model.addAttribute("listOwners", listOwners);
 
         return "owners/index";
     }

9e35bcf228b1e6346df24c2947be36f33620a212
Okan Hollander <o.hollander1990@gmail.com>
2019-12-29 12:04:06 +0100

    Adding index page and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/com/okan/petclinic/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.okan.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Author:   Okan Hollander
+ * Date:     29/12/2019
+ * Time:     11:55
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+}

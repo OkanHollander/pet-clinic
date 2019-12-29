f274f94f2b8abe466bfbe3c362ac4efc9bbc14ef
Okan Hollander <o.hollander1990@gmail.com>
2019-12-29 21:18:17 +0100

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/okan/petclinic/services/PetTypeService.java
@@ -0,0 +1,12 @@
+package com.okan.petclinic.services;
+
+import com.okan.petclinic.model.PetType;
+
+/**
+ * Author:   Okan Hollander
+ * Date:     29/12/2019
+ * Time:     21:11
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/okan/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,42 @@
+package com.okan.petclinic.services.map;
+
+import com.okan.petclinic.model.PetType;
+import com.okan.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Author:   Okan Hollander
+ * Date:     29/12/2019
+ * Time:     21:12
+ */
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}

d0fc2be4e067bd1424eb7727c0036c181efcbdd4
Okan Hollander <o.hollander1990@gmail.com>
2019-12-29 13:19:06 +0100

    adding list of vets to vets index page Closes #15

--- pet-clinic-web/src/main/java/com/okan/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/okan/petclinic/controllers/VetController.java
@@ -1,8 +1,14 @@
 package com.okan.petclinic.controllers;
 
+import com.okan.petclinic.model.Vet;
+import com.okan.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 /**
  * Author:   Okan Hollander
@@ -13,9 +19,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @GetMapping({"", "/", "index", "index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        Set<Vet> listVets = vetService.findAll();
+
+        model.addAttribute("listVets", listVets);
 
         return "vets/index";
     }

361637943fe66ec81a38181c9f22397ea64fe279
Okan Hollander <o.hollander1990@gmail.com>
2019-12-30 09:47:48 +0100

    Added Speciality Service Map. Closes #31

---
+++ pet-clinic-data/src/main/java/com/okan/petclinic/services/SpecialityService.java
@@ -0,0 +1,13 @@
+package com.okan.petclinic.services;
+
+import com.okan.petclinic.model.Speciality;
+
+/**
+ * Author:   Okan Hollander
+ * Date:     30/12/2019
+ * Time:     09:33
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+
+}

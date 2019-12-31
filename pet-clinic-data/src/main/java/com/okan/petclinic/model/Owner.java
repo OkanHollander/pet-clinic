d74899302e2aaf1555fd4242da16bb5096b3dbb2
Okan Hollander <o.hollander1990@gmail.com>
2019-12-31 10:32:00 +0100

    updated owner with JPa entity mappings. Closes #36

--- pet-clinic-data/src/main/java/com/okan/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/okan/petclinic/model/Owner.java
@@ -1,15 +1,29 @@
 package com.okan.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    // == Fields ==
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    @Column(name = "pets")
     private Set<Pet> pets = new HashSet<>();
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
 
+
+    // == Getters and Setters ==
     public Set<Pet> getPets() {
         return pets;
     }

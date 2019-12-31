ee47722b9d63cf3ef6f59fceaf99e7387ac9935b
Okan Hollander <o.hollander1990@gmail.com>
2019-12-31 11:43:10 +0100

    added spring data jpa service for vets. Closes #41

---
+++ pet-clinic-data/src/main/java/com/okan/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,57 @@
+package com.okan.petclinic.services.springdatajpa;
+
+import com.okan.petclinic.model.Vet;
+import com.okan.petclinic.repositories.VetRepository;
+import com.okan.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Author:   Okan Hollander
+ * Date:     31/12/2019
+ * Time:     11:34
+ */
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    @Autowired
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+
+        vetRepository.findAll().forEach(vets::add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}

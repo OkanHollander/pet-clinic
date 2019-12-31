f041e93bfa36325c67995e72108564c7ccb13227
Okan Hollander <o.hollander1990@gmail.com>
2019-12-31 10:44:47 +0100

    updated vet with JPA. Closes #37

--- pet-clinic-data/src/main/java/com/okan/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/okan/petclinic/model/Speciality.java
@@ -1,12 +1,19 @@
 package com.okan.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Author:   Okan Hollander
  * Date:     29/12/2019
  * Time:     21:03
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {

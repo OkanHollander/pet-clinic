809f64143095dd703c834eea594d60e3ef398647
Okan Hollander <o.hollander1990@gmail.com>
2019-12-31 12:38:02 +0100

    Implemented Map impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/okan/petclinic/services/VisitService.java
@@ -0,0 +1,12 @@
+package com.okan.petclinic.services;
+
+import com.okan.petclinic.model.Visit;
+
+/**
+ * Author:   Okan Hollander
+ * Date:     31/12/2019
+ * Time:     12:25
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+
+}

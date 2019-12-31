d3b3a9aea0915e2df9b4346f468956cccc80f3b6
Okan Hollander <o.hollander1990@gmail.com>
2019-12-31 14:57:29 +0100

    Added Project Lombok. Closes #51

--- pet-clinic-data/src/main/java/com/okan/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/okan/petclinic/model/Speciality.java
@@ -1,5 +1,7 @@
 package com.okan.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -9,6 +11,12 @@ import javax.persistence.Table;
  * Date:     29/12/2019
  * Time:     21:03
  */
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialities")
 public class Speciality extends BaseEntity{
@@ -16,11 +24,4 @@ public class Speciality extends BaseEntity{
     @Column(name = "description")
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- pet-clinic-data/src/main/java/com/okan/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/okan/petclinic/model/Visit.java
@@ -1,5 +1,7 @@
 package com.okan.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
@@ -8,6 +10,12 @@ import java.time.LocalDate;
  * Date:     29/12/2019
  * Time:     20:59
  */
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
@@ -23,29 +31,4 @@ public class Visit extends BaseEntity {
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
-
-    // Getters and Setters
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }

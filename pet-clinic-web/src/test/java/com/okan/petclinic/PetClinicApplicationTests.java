f93058d8e1c30006f1b8d95f85179b27626d1a57
Okan Hollander <o.hollander1990@gmail.com>
2020-01-01 12:28:47 +0100

    JUnit5

--- pet-clinic-web/src/test/java/com/okan/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/okan/petclinic/PetClinicApplicationTests.java
@@ -1,13 +1,17 @@
 package com.okan.petclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class PetClinicApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }

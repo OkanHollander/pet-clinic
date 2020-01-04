ae268c8d3c76c186c6115b056030038ad194e250
Okan Hollander <o.hollander1990@gmail.com>
2020-01-04 14:21:56 +0100

    display owner records Closes #53

--- pet-clinic-web/src/main/java/com/okan/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/okan/petclinic/controllers/OwnerController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Set;
 
@@ -43,4 +45,14 @@ public class OwnerController {
     public String findOwners() {
         return "notimplemented";
     }
+
+    // different implementation to use model and view at the same time
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+
+        return mav;
+    }
+
 }

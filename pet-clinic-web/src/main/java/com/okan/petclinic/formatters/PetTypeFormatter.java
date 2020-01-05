3015b7b2aba9be3565b64aeb1d29455835ee2183
Okan Hollander <o.hollander1990@gmail.com>
2020-01-05 13:07:22 +0100

    added PetTypeFormatter.
    This formatter is needed to fix the NumberFormatException
    
    Closes #67

---
+++ pet-clinic-web/src/main/java/com/okan/petclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,43 @@
+package com.okan.petclinic.formatters;
+
+import com.okan.petclinic.model.PetType;
+import com.okan.petclinic.services.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+/**
+ * Author:   Okan Hollander
+ * Date:     05/01/2020
+ * Time:     12:55
+ */
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+}

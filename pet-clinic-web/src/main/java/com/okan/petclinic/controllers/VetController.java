3a80226708102c830d859a1bb886599c622d4308
Okan Hollander <o.hollander1990@gmail.com>
2020-01-06 08:56:56 +0100

    added json endpoint for vets. Closes #72

--- pet-clinic-web/src/main/java/com/okan/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/okan/petclinic/controllers/VetController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Set;
 
@@ -32,5 +33,12 @@ public class VetController {
         model.addAttribute("listVets", listVets);
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+
+        return vetService.findAll();
     }
 }
